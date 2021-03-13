0df7145fe40759a0b6e10533c6851332a49e65f9
Alice <alicepsouzaba1998@gmail.com>
2021-03-13 17:14:18 -0300

    structure components

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { FooterContainer } from './styles';
+
+const Footer: React.FC = () => {
+  return (
+    <FooterContainer>
+      Eu sou a página de Erro!
+    </FooterContainer>
+  )
+}
+
+export default Footer;
