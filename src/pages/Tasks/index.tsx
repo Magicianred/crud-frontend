8c647ef96b316fcf96f786f5aff957780727dee2
Alice <alicepsouzaba1998@gmail.com>
2021-03-13 18:31:13 -0300

    table tasks

--- src/pages/Tasks/index.tsx
+++ src/pages/Tasks/index.tsx
@@ -1,11 +1,43 @@
-import React from 'react';
-import { TasksPage } from './styles'
+import React, { useState, useEffect } from 'react';
+import { Table } from 'react-bootstrap';
 
 const Tasks: React.FC = () => {
   return (
-    <TasksPage>
-      Tasks page
-    </TasksPage>
+    <div className="container">
+      <br></br>
+      <h2>Tasks</h2>
+      <br></br>
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Username</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+        </tbody>
+      </Table>
+    </div>
   )
 }
 
