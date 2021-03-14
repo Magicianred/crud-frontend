087c3a8201957627debd15e3d3e4cd5764ea6bfa
Alice <alicepsouzaba1998@gmail.com>
2021-03-14 16:59:49 -0300

    finished home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,18 @@
 import React from 'react'
+import { useHistory } from 'react-router-dom';
 import { HomePage } from './styles';
 
 const Home: React.FC = () => {
+  const history = useHistory()
+
+  const goToCreateTask = async () => {
+    history.push('/cadastro')
+  }
+
   return (
     <HomePage>
-      Eu sou a Home!
+      <h1>Bem-vindo(a) ao CreativeTask!</h1>
+      <h2><u onClick={goToCreateTask}>Clique aqui</u> para criar sua tarefa!</h2>
     </HomePage>
   )
 }
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,5 +1,13 @@
 import styled from 'styled-components';
 
 export const HomePage = styled.div`
-
+  text-align: center;
+  padding-top: 100px;
+  color: #575757;
+  h2 {  
+    u {
+      cursor: pointer;
+      color: #0011ff;
+    }
+  }
 `
