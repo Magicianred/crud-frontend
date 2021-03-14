a2880dcdb08fb15b265e225acccd6ece198b3e95
Alice <alicepsouzaba1998@gmail.com>
2021-03-14 15:09:05 -0300

    new route (details task)

--- src/router/Routes.tsx
+++ src/router/Routes.tsx
@@ -3,6 +3,7 @@ import { Switch, Route } from 'react-router-dom';
 import Home from '../pages/Home/index';
 import Tasks from '../pages/Tasks/index';
 import TaskForm from '../pages/TaskForm/index';
+import TaskDetails from '../pages/TaskDetails/index';
 import Error from '../pages/Error/index';
 
 const Routes: React.FC = () => {
@@ -12,6 +13,7 @@ const Routes: React.FC = () => {
       <Route path="/tarefas" exact component={Tasks} />
       <Route path="/cadastro" exact component={TaskForm} />
       <Route path="/cadastro/:id" exact component={TaskForm} />
+      <Route path="/detalhes/:id" exact component={TaskDetails} />
       <Error />
     </Switch>
   )
